786365665d28decac5d2ebc2db86e94a910ffc05
Daniel Kenji <daniel_kenji_1@hotmail.com>
2017-10-16 19:24:09 -0200

    Created class domain Item

---
+++ TodoList/Domain/Item.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.Domain
+{
+    public class Item
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public Boolean IsFinished { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public DateTime? DeletedAt { get; set; }
+
+        public Guid TaskId { get; set; }
+
+        #region Navigation
+
+        public virtual Task Task { get; set; }
+
+        #endregion Navigation
+    }
+}
