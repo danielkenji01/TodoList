045e71956d705c3b083182711a7c12ddec245c48
Daniel Kenji <daniel_kenji_1@hotmail.com>
2017-10-16 19:23:58 -0200

    Created class Db

---
+++ TodoList/Infraestructure/Db.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Domain;
+
+namespace TodoList.Infraestructure
+{
+    public class Db : DbContext
+    {
+        public Db(DbContextOptions<Db> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder m)
+        {
+            base.OnModelCreating(m);
+
+            m.Entity<Item>().ToTable(nameof(Item));
+            m.Entity<Domain.Task>().ToTable(nameof(Domain.Task));
+
+            m.Entity<Domain.Task>().HasMany(t => t.Itens).WithOne(t => t.Task).HasForeignKey(t => t.TaskId);
+            m.Entity<Domain.Task>().Property(t => t.CreatedAt).ValueGeneratedOnAdd();
+            m.Entity<Domain.Task>().Property(t => t.UpdatedAt).ValueGeneratedOnAddOrUpdate();
+
+            m.Entity<Item>().HasOne(i => i.Task).WithMany(i => i.Itens).HasForeignKey(i => i.TaskId);
+            m.Entity<Item>().Property(t => t.CreatedAt).ValueGeneratedOnAdd();
+            m.Entity<Item>().Property(t => t.UpdatedAt).ValueGeneratedOnAddOrUpdate();
+        }
+    }
+}
