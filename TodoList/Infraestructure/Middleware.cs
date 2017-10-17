4f26ad4ffc765b8f1e92e01577aa66455f8badc3
Daniel Kenji <daniel_kenji_1@hotmail.com>
2017-10-17 19:33:34 -0200

    Middleware

---
+++ TodoList/Infraestructure/Middleware.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace TodoList.Infraestructure
+{
+    public class Middleware
+    {
+        private readonly RequestDelegate next;
+
+        public Middleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await this.next(context);
+            }
+            catch (HttpException e)
+            {
+                context.Response.StatusCode = e.StatusCode;
+                if (e.Body != null)
+                {
+                }
+            }
+        }
+    }
+}
