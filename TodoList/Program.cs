d80e71509606622e8ec880a04c0667bd99609a97
Daniel Kenji <daniel_kenji_1@hotmail.com>
2017-10-17 19:33:02 -0200

    UseKestrel and UseIISIntegration

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -19,7 +19,9 @@ namespace TodoList
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+            .UseKestrel()
+            .UseIISIntegration()
+            .UseStartup<Startup>()
+            .Build();
     }
 }
