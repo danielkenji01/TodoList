b7c6ff6dedf67930d8d95fb8b4cab44c4d567086
Daniel Kenji <daniel_kenji_1@hotmail.com>
2017-10-17 14:50:12 -0200

    class Update from Assignment created

---
+++ TodoList/Features/Assignment/Update.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Infraestructure;
+
+namespace TodoList.Features.Assignment
+{
+    public class Update
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+
+            public string Name { get; set; }
+
+            public string Description { get; set; }
+        }
+
+        public class CommandHandler : IAsyncRequestHandler<Command>
+        {
+            private readonly Db db;
+
+            public CommandHandler(Db db)
+            {
+                this.db = db;
+            }
+
+            public async Task Handle(Command message)
+            {
+                var assignment = await db.Assignment.FindAsync(message.Id);
+
+                if (message.Name != null) assignment.Name = message.Name;
+
+                if (message.Description != null) assignment.Description = message.Description;
+
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+}
