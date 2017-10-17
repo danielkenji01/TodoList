680f189bd2c728bb080ff984fb53c2919080651b
Daniel Kenji <daniel_kenji_1@hotmail.com>
2017-10-17 13:59:45 -0200

    Endpoint List created

--- TodoList/Features/Assignment/AssignmentController.cs
+++ TodoList/Features/Assignment/AssignmentController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MediatR;
+using TodoList.Features.Assignment;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -25,6 +26,14 @@ namespace TodoList.Features.Task
             await mediator.Send(command);
 
             return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IList<List.Result>> List()
+        {
+            var result = await mediator.Send(new List.Query());
+
+            return result;
         }
     }
 }
