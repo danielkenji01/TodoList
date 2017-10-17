627abb6f20504ffa59347b03c847239dc3548f45
Daniel Kenji <daniel_kenji_1@hotmail.com>
2017-10-17 15:16:22 -0200

    class Delete from Item created

---
+++ TodoList/Features/Item/Delete.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Infraestructure;
+
+namespace TodoList.Features.Item
+{
+    public class Delete
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class CommandHandler : IAsyncRequestHandler<Command>
+        {
+            private readonly Db db;
+
+            public CommandHandler(Db db)
+            {
+                this.db = db;
+            }
+
+            public async Task Handle(Command message)
+            {
+                var item = await db.Item.FindAsync(message.Id);
+
+                item.DeletedAt = DateTime.Now;
+
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+}
