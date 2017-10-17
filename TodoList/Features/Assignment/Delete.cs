cc0402a4eba453445c17336cb2a1544bbbecac71
Daniel Kenji <daniel_kenji_1@hotmail.com>
2017-10-17 14:18:59 -0200

    class Delete from Assingment created

---
+++ TodoList/Features/Assignment/Delete.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Infraestructure;
+
+namespace TodoList.Features.Assignment
+{
+    public class Delete
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IAsyncRequestHandler<Command>
+        {
+            private readonly Db db;
+
+            public Handler(Db db)
+            {
+                this.db = db;
+            }
+
+            public async Task Handle(Command message)
+            {
+                var assignment = await db.Assignment.FindAsync(message.Id);
+
+                assignment.DeletedAt = DateTime.Now;
+
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+}
