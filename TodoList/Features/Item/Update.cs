2521c46f73342239fda3dd038d093530349218a0
Daniel Kenji <daniel_kenji_1@hotmail.com>
2017-10-17 15:25:18 -0200

    class Update from Item created

---
+++ TodoList/Features/Item/Update.cs
@@ -0,0 +1,46 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Infraestructure;
+
+namespace TodoList.Features.Item
+{
+    public class Update
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+
+            public string Name { get; set; }
+
+            public string Description { get; set; }
+
+            public Boolean IsFinished { get; set; }
+        }
+
+        public class CommandHandler : IAsyncRequestHandler<Command>
+        {
+            private readonly Db db;
+
+            public CommandHandler(Db db)
+            {
+                this.db = db;
+            }
+
+            public async Task Handle(Command message)
+            {
+                var item = await db.Item.FindAsync(message.Id);
+
+                if (message.Name != null) item.Name = message.Name;
+
+                if (message.IsFinished.ToString() != null) item.IsFinished = message.IsFinished;
+
+                if (message.Description != null) item.Description = message.Description;
+
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+}
