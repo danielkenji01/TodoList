54f48ec4ea5f6577502b2e79ff9097478545a09f
Daniel Kenji <daniel_kenji_1@hotmail.com>
2017-10-17 15:07:21 -0200

    class Read from Item created

---
+++ TodoList/Features/Item/Read.cs
@@ -0,0 +1,57 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Infraestructure;
+
+namespace TodoList.Features.Item
+{
+    public class Read
+    {
+        public class Query : IRequest<Result>
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Result
+        {
+            public Guid Id { get; set; }
+
+            public Guid AssignmentId { get; set; }
+
+            public string Name { get; set; }
+
+            public string Description { get; set; }
+
+            public Boolean IsFinished { get; set; }
+
+            public DateTime CreatedAt { get; set; }
+        }
+
+        public class QueryHandler : IAsyncRequestHandler<Query, Result>
+        {
+            private readonly Db db;
+
+            public QueryHandler(Db db)
+            {
+                this.db = db;
+            }
+
+            public async Task<Result> Handle(Query message)
+            {
+                var item = await db.Item.FindAsync(message.Id);
+
+                return new Result
+                {
+                    Id = item.Id,
+                    Name = item.Name,
+                    Description = item.Description,
+                    IsFinished = item.IsFinished,
+                    CreatedAt = item.CreatedAt,
+                    AssignmentId = item.AssignmentId
+                };
+            }
+        }
+    }
+}
