4c44244ec0e41184dae708620ad8e527e9a3c73c
Daniel Kenji <daniel_kenji_1@hotmail.com>
2017-10-17 19:33:26 -0200

    PropagatingExceptions

---
+++ TodoList/Infraestructure/PropagatingExceptions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.Infraestructure
+{
+    public class HttpException : Exception
+    {
+        public int StatusCode { get; set; }
+
+        public dynamic Body { get; set; }
+
+        public HttpException(int statusCode)
+        {
+            this.StatusCode = statusCode;
+        }
+
+        public HttpException(int statusCode, dynamic body)
+        {
+            this.StatusCode = statusCode;
+            this.Body = body;
+        }
+    }
+
+    public class NotFoundException : HttpException
+    {
+        public NotFoundException() : base(400)
+        {
+        }
+    }
+}
