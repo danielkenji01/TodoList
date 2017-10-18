ed095cc615c06e9127c4485bb0708fdb19fea914
Daniel Kenji <daniel_kenji_1@hotmail.com>
2017-10-18 17:04:35 -0200

    class AssignmentItem created

---
+++ TodoList/Features/Assignment/AssignmentItem.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Infraestructure;
+
+namespace TodoList.Features.Assignment
+{
+    public class AssignmentItem
+    {
+        public class Command : IRequest
+        {
+            public Guid AssignmentId { get; set; }
+
+            public Guid ItemId { get; set; }
+        }
+
+        public class CommandHandler : IAsyncRequestHandler<Command>
+        {
+            private readonly Db db;
+
+            public CommandHandler(Db db)
+            {
+                this.db = db;
+            }
+
+            public async Task Handle(Command message)
+            {
+                var assignmentItem = new Domain.Assignment_Item()
+                {
+                    AssignmentId = message.AssignmentId,
+                    ItemId = message.ItemId
+                };
+
+                db.Assignment_Item.Add(assignmentItem);
+
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+}
