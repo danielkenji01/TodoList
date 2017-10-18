1f05ab22675f9276920b9b67a17484580265225a
Daniel Kenji <daniel_kenji_1@hotmail.com>
2017-10-18 17:40:26 -0200

    list from assignment must return all itens

--- TodoList/Features/Assignment/List.cs
+++ TodoList/Features/Assignment/List.cs
@@ -23,6 +23,19 @@ namespace TodoList.Features.Assignment
             public string Description { get; set; }
 
             public DateTime CreatedAt { get; set; }
+
+            public SubResult[] Itens { get; set; }
+
+            public class SubResult
+            {
+                public Guid Id { get; set; }
+
+                public string Name { get; set; }
+
+                public string Description { get; set; }
+
+                public Boolean IsFinished { get; set; }
+            }
         }
 
         public class QueryHandler : IAsyncRequestHandler<Query, IList<Result>>
@@ -37,13 +50,22 @@ namespace TodoList.Features.Assignment
             public async Task<IList<Result>> Handle(Query message)
             {
                 return await db.Assignment
+                    .Include(a => a.Assignment_Item)
+                    .ThenInclude(ai => ai.Item)
                     .Where(a => a.DeletedAt == null)
                     .Select(a => new Result
                     {
                         Id = a.Id,
                         Name = a.Name,
                         Description = a.Description,
-                        CreatedAt = a.CreatedAt
+                        CreatedAt = a.CreatedAt,
+                        Itens = a.Assignment_Item.Where(ai => !ai.Item.DeletedAt.HasValue).Select(i => new Result.SubResult
+                        {
+                            Id = i.Item.Id,
+                            Name = i.Item.Name,
+                            Description = i.Item.Description,
+                            IsFinished = i.Item.IsFinished
+                        }).ToArray()
                     }).ToListAsync();
             }
         }
