e482670a2e7e33f998d35a73bc56a7a66fda4d05
Daniel Kenji <daniel_kenji_1@hotmail.com>
2017-10-18 14:52:43 -0200

    removing unecessary packages in read

--- TodoList/Features/Assignment/Read.cs
+++ TodoList/Features/Assignment/Read.cs
@@ -1,10 +1,7 @@
 ﻿using MediatR;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using TodoList.Infraestructure;
-using static TodoList.Infraestructure.HttpException;
 
 namespace TodoList.Features.Assignment
 {
