1d4e111e77cc9219b2609efea00907575fd69129
Daniel Kenji <daniel_kenji_1@hotmail.com>
2017-10-18 16:56:34 -0200

    class domain Assignment_Item added

---
+++ TodoList/Domain/Assignment_Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.Domain
+{
+    public class Assignment_Item
+    {
+        public Guid AssignmentId { get; set; }
+
+        public Guid ItemId { get; set; }
+
+        #region Navigation
+
+        public virtual Assignment Assignment { get; set; }
+
+        public virtual Item Item { get; set; }
+
+        #endregion Navigation
+    }
+}
