ccb1f47d5df024b88e841f5aaf7d1076d970ee54
Daniel Kenji <daniel_kenji_1@hotmail.com>
2017-10-18 17:12:49 -0200

    class DeleteAssignmentItem created

---
+++ TodoList/Features/Assignment/DeleteAssignmentItem.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Infraestructure;
+
+namespace TodoList.Features.Assignment
+{
+    public class DeleteAssignmentItem
+    {
+        public class Command : IRequest
+        {
+            public Guid AssignmentId { get; set; }
+
+            public Guid ItemId { get; set; }
+        }
+
+        public class CommandHandler : IAsyncRequestHandler<Command>
+        {
+            private readonly Db db;
+
+            public CommandHandler(Db db)
+            {
+                this.db = db;
+            }
+
+            public async Task Handle(Command message)
+            {
+                var assignment = db.Assignment
+                    .Include(a => a.Assignment_Item)
+                    .SingleOrDefault(a => a.Id == message.AssignmentId);
+
+                db.Assignment_Item.RemoveRange(assignment.Assignment_Item.Where(ai => ai.ItemId == message.ItemId));
+
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+}
