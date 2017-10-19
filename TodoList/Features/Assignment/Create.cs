d0380955b0a6bbb872b2f5c3646664581935f28a
Daniel Kenji <daniel_kenji_1@hotmail.com>
2017-10-19 14:17:57 -0200

    technical adjust for create from Assignment

--- TodoList/Features/Assignment/Create.cs
+++ TodoList/Features/Assignment/Create.cs
@@ -34,7 +34,9 @@ namespace TodoList.Features.Assignment
 
             public async Task Handle(Command message)
             {
-                if (message.Description.Trim().Equals("") || message.Name.Trim().Equals("")) throw new HttpException(400);
+                if (message.Description == null || message.Description.Trim().Equals("")) throw new HttpException(400);
+
+                if (message.Name == null || message.Name.Trim().Equals("")) throw new HttpException(400);
 
                 var assignment = new Domain.Assignment()
                 {
